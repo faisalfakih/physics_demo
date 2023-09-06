f63fc66188ff055c65ac814837e944490f7f97ab
Faisal Fakih <faisal.m.fakih.123@gmail.com>
2023-09-06 19:56:03 +0400

    1.0.1

--- sketch.js
+++ sketch.js
@@ -1,5 +1,13 @@
+let blocks;
+
 function setup() {
-	new Canvas(250, 100);
+	new Canvas(400, 400);
+	world.gravity.y = 10;
+
+	blocks = new Group();
+	blocks.w = 100;
+	blocks.color = 'red';
+	blocks.h = 80;
 }
 
 function draw() {
